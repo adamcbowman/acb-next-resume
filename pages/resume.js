8710cc36169018f29d8316bf90e1d9aed3bf3aec
Adam Bowman <adamcbowman@gmail.com>
2022-06-01 07:59:08 -0300

    work section styling

--- pages/resume.js
+++ pages/resume.js
@@ -22,7 +22,7 @@ const Resume = () => {
   return (
     <div>
       <Header />
-      <div className="text-center">
+      <div className="">
         {!resume && (
           <div className="m-12">
             <h1>Fetching data...</h1>
@@ -31,7 +31,7 @@ const Resume = () => {
 
         {resume && (
           <>
-            <div className="hero bg-base-200 pt-6">
+            <div className="hero bg-base-200 p-20">
               <div className="hero-content text-center">
                 <div className="max-w-md">
                   <div className="avatar p-6">
@@ -55,7 +55,61 @@ const Resume = () => {
               </div>
             </div>
 
-            <div className="row pt-6 border">
+            <div className="bg-white shadow overflow-hidden sm:rounded-lg w-full items-center">
+              <div className="px-4 py-5 sm:px-6">
+                <div className="mt-8 text-center">
+                <h3 className="text-lg leading-6 font-semibold text-gray-900">
+                  Work Experience
+                </h3>
+                </div>
+                
+              </div>
+              <div className = "md:flex">
+              {resume.work.map((work, index) => (
+                <>
+              <div className="border border-gray-200 md:w-1/2 m-6 rounded-lg">
+                <dl>
+                  <div className="bg-gray-50 px-2 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      Company
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      {work.company}
+                    </dd>
+                  </div>
+                  <div className="bg-white px-2 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      position
+                    </dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                      {work.position} <br />
+                      {work.startDate} - {work.endDate}
+                    </dd>
+                  </div>
+                  <div className="bg-gray-50 px-2 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">
+                      website
+                    </dt>
+                    <dd className="mt-1 text-sm text-primary sm:mt-0 sm:col-span-2">
+                      <a href={work.website} target="_blank" rel="noreferrer">{work.website}</a>
+                    </dd>
+                  </div>
+                  
+                  <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt className="text-sm font-medium text-gray-500">About</dt>
+                    <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 text-left">
+                      {work.summary}
+                    </dd>
+                  </div>
+                  
+                </dl>
+              </div>
+              </>
+              ))}
+            </div>
+            </div>
+
+            {/* <div className="row pt-6 border">
               <div className="col-md-12">
                 <h2 className="text-xl font-semibold underline">
                   Work Experience
@@ -71,7 +125,7 @@ const Resume = () => {
                   </div>
                 ))}
               </div>
-            </div>
+            </div> */}
 
             <div className="row pt-6 border">
               <div className="col-md-12">
